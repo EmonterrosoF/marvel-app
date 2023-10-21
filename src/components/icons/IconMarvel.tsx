b71a37e1a37742aab31e3f21bf927cb5bbfd5bd6
Edras <monterrosoedras@gmail.com>
2023-10-21 13:28:17 -0600

    add icons

--- src/components/icons/IconMarvel.tsx
+++ src/components/icons/IconMarvel.tsx
@@ -11,18 +11,18 @@ export const IconMarvel = () => {
       <g transform="translate(-75.52,-75.52) scale(1.59,1.59)">
         <g
           fill="none"
-          fill-rule="nonzero"
+          fillRule="nonzero"
           stroke="none"
-          stroke-width="1"
-          stroke-linecap="butt"
-          stroke-linejoin="miter"
-          stroke-miterlimit="10"
-          stroke-dasharray=""
-          stroke-dashoffset="0"
-          font-family="none"
-          font-weight="none"
-          font-size="none"
-          text-anchor="none"
+          strokeWidth="1"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit="10"
+          strokeDasharray=""
+          strokeDashoffset="0"
+          fontFamily="none"
+          fontWeight="none"
+          fontSize="none"
+          textAnchor="none"
         >
           <g transform="scale(5.33333,5.33333)">
             <rect x="0" y="14" width="48" height="20" fill="#d32f2f"></rect>
